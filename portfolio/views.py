b4fbda5ec1d4b6df8c3613b1ef3e11f6ae6be418
Suhair K <67942109+Suhairk@users.noreply.github.com>
2021-10-14 11:57:42 +0530

    Updated views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,7 @@
-import os
 import mimetypes
-import re
 from django.db import models
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from . models import projects,form
 
 # Create your views here.
@@ -17,22 +15,30 @@ def home(request):
         name=request.POST['name']
         email=request.POST['email']
         phone=request.POST['phone']
-        message=request.POST['message']        
+        message=request.POST['message']
 
         #print(name, email, phone ,message)
         ins=form(name=name , email= email , phone=phone ,message=message)
-        
+
         ins.save()
         #print("data saved")
     return render(request , 'index.html',{'project':project})
 
-def details(request):
-    return render(request ,'portfolio-details.html',{'project':project})
 
-def download(request):
+def download_res(request):
     # fill these variables with real values
-    fl_path = 'media/ExpResume.pdf'
-    filename = 'download.pdf'
+    fl_path = '/home/imsuhair/django_website/media/Suhair_Ai-python.pdf'
+    filename = 'Res_suhair_python_ai.pdf'
+    fl = open(fl_path, 'rb')
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    response = HttpResponse(fl, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
+
+def download_cover(request):
+    # fill these variables with real values
+    fl_path = '/home/imsuhair/django_website/media/Suhair_coverLetter.pdf'
+    filename = 'Cov_suhair_Ai_python.pdf'
     fl = open(fl_path, 'rb')
     mime_type, _ = mimetypes.guess_type(fl_path)
     response = HttpResponse(fl, content_type=mime_type)
